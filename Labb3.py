c8e74cc7fd6da8ccb451c289330c8e41d0c111e9
Rohini Singh <rohiniv@chalmers.se>
2026-02-19 10:48:02 +0100

    matris

--- Labb3.py
+++ Labb3.py
@@ -3,8 +3,28 @@ import matplotlib as mpl
 
 def uppgift1(punkter):
     
-    #Dela upp x och y
-    a = punkter [:, 0]
-    b = punkter [:, 1]
+    n = len (punkter)
+
+    #Tomma matriser
+    A = np.zeros((n, 4))
+    Y = np.zeros((n, 1))
+
+    # Loopa igenom varje punkt
+    for j in range(n):
+
+        # Plocka ut a_j och b_j direkt
+        a_j = punkter[j, 0]
+        b_j = punkter[j, 1]
+
+        # Fyll A enligt uppgiften
+        A[j, 0] = a_j**3
+        A[j, 1] = a_j**2
+        A[j, 2] = a_j
+        A[j, 3] = 1
+
+        # Fyll Y
+        Y[j, 0] = b_j
+
+
 
 
